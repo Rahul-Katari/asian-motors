6dccfb20417344dd6e6727ac64a3c4b4131e1569
Rahul Katari <rahulkatari99@gmail.com>
2024-11-28 22:02:46 +0530

    commented emi calculator

--- src/components/pages/EmiCalculatorPage.js
+++ src/components/pages/EmiCalculatorPage.js
@@ -12,7 +12,7 @@ const EmiCalculatorPage = () => {
       <section className="contact-us-section layout-radius">
         <div className="boxcar-container">
           <Breadcrumb page={'EMI Calculator'}/>
-          <EmiCalculator/>
+          {/* <EmiCalculator/> */}
         </div>
       </section>
     </>
