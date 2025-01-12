af2079acf36bbedb1812368bca85224e882cf782
Rahul Katari <rahulkatari99@gmail.com>
2025-01-12 13:03:29 +0530

    modified google tag.

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,13 +29,13 @@ export default function RootLayout({ children }) {
       <head>
         <script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T3G0PV409Y"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-603857874"></script>
         <script dangerouslySetInnerHTML={{
           __html: `
            window.dataLayer = window.dataLayer || [];
            function gtag(){dataLayer.push(arguments);}
            gtag('js', new Date());
-           gtag('config', 'G-T3G0PV409Y');
+           gtag('config', 'AW-603857874');
          `,
 
         }}>
